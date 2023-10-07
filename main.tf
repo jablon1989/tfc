206b60e92d9bdb2301ac517868e9db72f4551ff8
Bartosz Jablonski <bartosz.jablonski@cpexterne.org>
2023-10-07 10:54:39 +0200

    changed data aws ami

--- main.tf
+++ main.tf
@@ -89,8 +89,24 @@ resource "aws_eip_association" "test-eip-association" {
   allocation_id = aws_eip.test-eip.id
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "test-ec2" {
-  ami                         = "ami-067d1e60475437da2"
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   associate_public_ip_address = true
   subnet_id                   = element(module.vpc.public_subnets, 0)
