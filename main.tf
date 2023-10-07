fd2db2f465bfbe8396f6c2734721ac520776bb83
Bartosz Jablonski <bartosz.jablonski@cpexterne.org>
2023-10-07 10:50:59 +0200

    changed data aws ami

--- main.tf
+++ main.tf
@@ -77,19 +77,10 @@ resource "aws_security_group" "test-security-group" {
 }
 
 data "aws_ami" "ubuntu" {
+  executable_users = ["self"]
   most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+  name_regex = "ami-067d1e60475437da2"
+  owners = ["self"]
 }
 
 resource "aws_eip" "test-eip" {
