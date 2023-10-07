c9020b2857013a83133aa89705f3cde23245e8a6
Bartosz Jablonski <bartosz.jablonski@cpexterne.org>
2023-10-07 10:58:44 +0200

    changes to ec2

--- main.tf
+++ main.tf
@@ -18,77 +18,6 @@ provider "aws" {
   }
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "my-vpc-${var.environment}"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
-
-resource "aws_security_group" "test-security-group" {
-  description = "Security Group Access"
-  name        = "${var.prefix}-security-group"
-
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8001
-    to_port     = 8001
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.prefix}-security-group"
-  }
-}
-
-resource "aws_eip" "test-eip" {
-  instance = aws_instance.test-ec2.id
-  vpc      = true
-  tags = {
-    "Name" = "${var.prefix}-${var.project}-${var.environment}"
-  }
-}
-
-resource "aws_eip_association" "test-eip-association" {
-  instance_id   = aws_instance.test-ec2.id
-  allocation_id = aws_eip.test-eip.id
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -108,10 +37,6 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "test-ec2" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
-  associate_public_ip_address = true
-  subnet_id                   = element(module.vpc.public_subnets, 0)
-  vpc_security_group_ids      = [aws_security_group.test-security-group.id]
-
   tags = {
     Name = "${var.prefix}-${var.project}-${var.environment}-instance"
   }
